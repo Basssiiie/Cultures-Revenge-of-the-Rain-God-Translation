206a94a63d928d5f31137066b5c7fe6b0b0cbbff
Bas <Basssiiie@users.noreply.github.com>
2023-10-11 22:20:06 +0200

    Add ci unit tests

---
+++ tests/IniFiles.Tests.ps1
@@ -0,0 +1,43 @@
+Import-Module Pester -Passthru
+
+BeforeDiscovery {
+    $files = Get-ChildItem "./translations/english/data_m/*.ini"
+}
+
+Describe "INI map string files" {
+	It "Files exist" -ForEach @{ files = $files } {
+		Write-Output $files
+		$files.Count | Should -BeGreaterThan 5
+
+		$filenames = $files | Select-Object -ExpandProperty Name
+		$filenames | Should -Contain "c2_01_a_txt.ini"
+		$filenames | Should -Contain "c1_md_05_txt.ini"
+	}
+
+    It "Valid ini file: <_.Name>" -ForEach $files {
+
+		$lines = Get-Content $_.FullName
+		$count = 1;
+		foreach ($line in $lines)
+		{
+			if($count -eq 1)
+			{
+				$line | Should -Be "[Text]"
+			}
+			elseif($count -gt 1 -and $line.StartsWith("sn "))
+			{
+				$line | Should -Match '^sn \d+ "[^"]*"$'
+			}
+			elseif($count -gt 2 -and $line.StartsWith("s "))
+			{
+				$line | Should -Match '^s "[^"]*"$'
+			}
+			else
+			{
+				$line | Should -Match '^\s*$'
+				$count | Should -BeGreaterThan 1
+			}
+			$count++
+		}
+    }
+}
